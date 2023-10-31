364c77d1d06ba08136df5e3caf079183c2ab01dd
Mariusflores <mada060@student.kristiania.no>
2023-10-31 09:03:39 +0100

    Initial commit

---
+++ src/main/java/com/example/diylesson10/DiyLesson10Application.java
@@ -0,0 +1,13 @@
+package com.example.diylesson10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyLesson10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyLesson10Application.class, args);
+	}
+
+}
